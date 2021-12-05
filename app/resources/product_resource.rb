715b5a31c4b46473ba7f56220f927eff090d428d
anonymous anonymous <kyle.graham@kellogg.northwestern.edu>
2021-12-05 18:19:55 +0000

    Generate graphiti resources for products

---
+++ app/resources/product_resource.rb
@@ -0,0 +1,15 @@
+class ProductResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :price, :float
+  attribute :description, :string
+  attribute :supplier_id, :integer
+  attribute :image, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
