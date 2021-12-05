8ebd13e5795fcd040a54d7e3cb60d449bc42a05b
anonymous anonymous <kyle.graham@kellogg.northwestern.edu>
2021-12-05 18:17:23 +0000

    Whitelist columns in Detail for ActiveAdmin

--- app/admin/details.rb
+++ app/admin/details.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Detail do
 
+ permit_params :project_id, :designer_id, :success_score, :description, :image, :name
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
