200a3bfecfcf554be1933b8f5420412715130cca
anonymous anonymous <kyle.graham@kellogg.northwestern.edu>
2021-12-05 18:20:01 +0000

    Generate graphiti resources for projects

---
+++ app/resources/project_resource.rb
@@ -0,0 +1,15 @@
+class ProjectResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :project_manager_id, :integer
+  attribute :location, :string
+  attribute :completion_date, :date
+  attribute :name, :string
+  attribute :description, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
