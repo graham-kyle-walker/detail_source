ffb53066b5642f49caa2867a1f963f07d22ac729
anonymous anonymous <kyle.graham@kellogg.northwestern.edu>
2021-12-05 18:17:20 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "details#index"
   resources :comments
   resources :saved_details
